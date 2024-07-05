fca24db3448409f05276ae9d29bcfdd9d2ba5074
ifarrag <ifarrrag@gmail.com>
2024-07-05 14:50:13 -0700

    string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    listString = []
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            listString.append(chr(ord(i) - 32))
+        else:
+            listString.append(i)
+    for n in range(len(listString) - 1):
+        print(listString[n], end="")
+    print()
