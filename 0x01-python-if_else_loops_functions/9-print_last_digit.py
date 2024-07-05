ede70a0c42deaf0ac0e414db06abe7cdc96c5129
ifarrag <ifarrrag@gmail.com>
2024-07-05 15:04:42 -0700

    Print the Last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if  number == 0:
+        print("{:d}".format(0))
+        return 0
+    elif number < 0:
+        number = -number
+        print("{:d}".format(number % 10))
+        return number % 10
+    else:
+        print("{:d}".format(number % 10))
+        return number % 10
