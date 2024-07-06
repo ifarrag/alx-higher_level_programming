3948ceacb924a3d2b06a73a8d9cd45a3da7756ed
ifarrag <ifarrrag@gmail.com>
2024-07-06 16:19:34 -0700

    0 arguments. or more

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) > 1:
+        if len(argv) == 2:
+            print("{:d} argument:".format(len(argv) - 1))
+        else:
+            print("{:d} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{0:d}: {1:s}".format(i, argv[i]))
+    else:
+        print("{:d} arguments.".format(0))
