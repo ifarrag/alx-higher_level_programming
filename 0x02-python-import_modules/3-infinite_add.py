3019c30de6f0523d42b1ec9f00d1c5a137281765
ifarrag <ifarrrag@gmail.com>
2024-07-06 16:33:39 -0700

    Oh My C -__- !

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     sum = 0
     if len(argv) > 1:
-        for i in range(1, len(argv):
+        for i in range(1, len(argv)):
                 sum = int(argv[i]) + sum
         print("{:d}".format(sum))
     else:
