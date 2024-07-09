304794ab4b0675ab9feba2ba3169bf254ce178ff
ifarrag <ifarrrag@gmail.com>
2024-07-09 16:39:27 -0700

    get item in the list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,6 +5,6 @@ def element_at(my_list, idx):
     else:
         for i in my_list:
             if i == idx:
-                print("Element at index {:d} is {:d}".format(i, my_list[i]))
+                return my_list[i]
         if i < idx:
             return "None"
