046cb874cf5370bcc7bcf12827b090dea9f2bfb8
ifarrag <ifarrrag@gmail.com>
2024-07-09 16:43:23 -0700

    get item in the list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     if idx < 0:
         return "None"
     else:
-        for i in my_list:
+        for i in range(len(my_list)):
             if i == idx:
                 return my_list[idx]
         if i < idx:
