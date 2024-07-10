4d4211f1ea8cedc9b6728f23f750fc07fd6aa364
ifarrag <ifarrrag@gmail.com>
2024-07-10 08:21:06 -0700

    Tupules is immutable

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,11 +2,16 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     new_t = []
     for i in range(2):
-        if len(tuple_a) < i:
+
+        if len(tuple_a) == 0 or tuple_a is None:
+            a = (0, 0)
+        if i > len(tuple_a):
             a = (0, 0)
         else:
             a = tuple_a
-        if len(tuple_b) < i:
+        if len(tuple_b) == 0 or tuple_b is None:
+            b = (0, 0)
+        elif i > len(tuple_b):
             b = (0, 0)
         else:
             b = tuple_b
