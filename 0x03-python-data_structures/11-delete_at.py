173426e1835211273223612b0b203b20b4a72af8
ifarrag <ifarrrag@gmail.com>
2024-07-10 11:08:57 -0700

    use [] to delete item

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,6 +5,6 @@ def delete_at(my_list=[], idx=0):
     else:
         for i in range(len(my_list)):
             if i == idx:
-                my_list[i] = []
+                my_list[i:i+1] = []
                 return my_list
         return my_list
