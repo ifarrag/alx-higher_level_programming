e507bc2264153343e125dadd4f5ddb9b3771fffc
ifarrag <ifarrrag@gmail.com>
2024-07-09 17:30:03 -0700

    Clear the {'c' and 'C'} from string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     string = []
     for i in range(len(my_string)):
-        if ('C' and 'c') not in my_string[i]:
+        if 'C' and 'c' not in my_string[i]:
             string.append(my_string[i])
     return string
