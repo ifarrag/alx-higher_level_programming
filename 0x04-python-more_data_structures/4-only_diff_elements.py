9f89b53ef29540f001996cf9a0f932becf722fe2
ifarrag <ifarrrag@gmail.com>
2024-07-12 11:11:04 -0700

    returns a set of all elements present in only one set

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     special = []
-    common = []
     for i in set(set_1):
         x = 0
         for n in set(set_2):
             if n == i:
                 x = 1
-        if x == 0
+        if x == 0:
             special.append(n)
     for i in set(set_2):
         x = 0
