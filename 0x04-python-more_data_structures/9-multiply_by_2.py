7ec472ade50b486cac470c649dfcacd9214b70ae
ifarrag <ifarrrag@gmail.com>
2024-07-12 12:15:39 -0700

    all values multiplied by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return dict{i:x*2 for i,x in a_dictionary.items()}
+    new_d = dict{i:x*2 for i,x in a_dictionary.items()}
+    return new_d
