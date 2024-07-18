47de543b8be169d0ecf6a05c74f43d982353fac2
ifarrag <ifarrrag@gmail.com>
2024-07-17 19:51:17 -0700

    ZeroDivisionError,finally:

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        res = a - b
+        res = a / b
         return res
-    except:
+    except ZeroDivisionError:
         res = None
         return None
     finally:
