fa8cd70b7b96ef0a80e85efea59aeee58875480b
ifarrag <ifarrrag@gmail.com>
2024-07-17 19:35:43 -0700

    TypeError, ValueError)

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     if x > 0:
-        divide = 0
+        sub = 0
         for i in range(x):
             try:
                 print("{:d}".format(my_list[i]), end="")
             except (ValueError, TypeError):
-                divide = divide + 1
+                sub = sub + 1
                 continue
         print()
-        return i + 1 - divide
+        return i + 1 - sub
     else:
         print()
         return 0
