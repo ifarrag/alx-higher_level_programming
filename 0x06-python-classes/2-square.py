86b63fa6cdf5a328a55f9e42745bf21510f10b15
ifarrag <ifarrrag@gmail.com>
2024-07-20 18:17:27 -0700

    raise exception if size < 0 or not int

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,12 +10,9 @@ class Square:
 """ Args: size
 Attributes: _Square__size
 raise Error if size not int or less than 0 """
-    n = 0
     def __init__(self, __size = 0):
         self._Square__size = __size
-    try:
-        n += self._Square__size
-    except Exception:
+    if type(__size) != int:
         raise TypeError("size must be an integer")
     if self._Square__size < 0:
         raise ValueError("size must be >= 0")
