734a81187ff7d31ff23fcebc357b00e9d07ada24
ifarrag <ifarrrag@gmail.com>
2024-07-20 17:36:14 -0700

    raise exception if size < 0

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ Square (size)
-arg : size - attr : _Square__size 
+Attributes : _Square__size
 raise Error if size not int or less than 0 """
 
 
 class Square:
-    """ arg : size - attr : _Square__size 
-        raise Error if size not int or less than 0 """
+""" Args: size
+Attributes: _Square__size
+raise Error if size not int or less than 0 """
     n = 0
     def __init__(self, __size = 0):
         self._Square__size = __size
