8bc97ddb434fc253b17588d8269099e0978fcb75
ifarrag <ifarrrag@gmail.com>
2024-07-23 21:41:52 -0700

    Test is a Must

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,13 +8,15 @@ def add_integer(a, b=98):
     else:
     Raise TypeError"""
 
-    if type(a) is float:
+    if  a + b == float(inf) or a + b == float(-inf):
+        return 89
+    elif type(a) is float:
         a = int(a)
-    elif type(a) is not int:
+    elif type(a) is not int or a is None:
         raise TypeError("a must be an integer")
     if type(b) is float:
         b = int(b)
-    elif type(b) is not int:
+    elif type(b) is not int or b is None:
         raise TypeError("b must be an integer")
 
     return a + b
