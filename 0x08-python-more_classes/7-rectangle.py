f0d052e21fd6022450c724d72b5c625659476384
ifarrag <ifarrrag@gmail.com>
2024-07-26 16:02:26 -0700

    Update 1.6

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,7 +6,7 @@
 class Rectangle:
     """ Define Rectangle """
     number_of_instances = 0
-    print_symbol = str(#)
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         self.height = height
