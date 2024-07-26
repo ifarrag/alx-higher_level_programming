c6ef11e8101c7cba01493658613a00ce26a8bd30
ifarrag <ifarrrag@gmail.com>
2024-07-26 06:43:50 -0700

    Property decorator

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -25,7 +25,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._Rectangle__height = value
+        self.__height = value
 
     @width.setter
     def width(self, value):
@@ -35,4 +35,4 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._Rectangle__width = value
+        self.__width = value
