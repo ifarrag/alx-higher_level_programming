908fd46c74323396ea278c720c80c7bd15e3d417
ifarrag <ifarrrag@gmail.com>
2024-07-26 17:24:08 -0700

    Update 1.8

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -17,10 +17,6 @@ class Rectangle:
     def width(self):
         return self.__width
 
-    @property
-    def height(self):
-        return self.__height
-
     @width.setter
     def width(self, value):
         if value is None:
@@ -30,6 +26,10 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
 
     @height.setter
     def height(self, value):
