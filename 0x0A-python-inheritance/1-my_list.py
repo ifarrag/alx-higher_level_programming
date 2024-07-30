45870ed750d91f0a4f52870f48956d5bf9333724
ifarrag <ifarrrag@gmail.com>
2024-07-30 10:30:02 -0700

    List obj

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,10 +4,6 @@
 
 class MyList(list):
 """    make list """
-    def __init__(self):
-        """  __init__ """
-        super().__init__()
-
     def print_sorted(self):
         """ print sorted"""
         self.sor = list.sort()
