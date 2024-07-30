8221134d5ee0bf877e378f8d6fe5bec4d4ee9ce6
ifarrag <ifarrrag@gmail.com>
2024-07-30 15:22:40 -0700

    List obj

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -10,6 +10,6 @@ class MyList(list):
 
     def print_sorted(self):
         """ print sorted"""
-        beSort = list()
-        beSort.sort()
-        print(beSort)
+        self.beSort = list()
+        self.beSort.sort()
+        print(self.beSort)
