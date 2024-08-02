cdcdd454bbb945ae8e6fbec1255282736e880119
ifarrag <ifarrrag@gmail.com>
2024-08-02 09:20:33 -0700

    open and read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" read()"""
+
+
 def read_file(filename=""):
+    """ return read() """
+
     with open(filename, encoding="utf-8") as f:
         print(f.read())
         return f.read()
