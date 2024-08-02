fa0b6a778c7d2a97b781ce9392e616f9962c5208
ifarrag <ifarrrag@gmail.com>
2024-08-02 09:35:40 -0700

    open-read-write-append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,6 +4,6 @@
 
 def append_write(filename="", text=""):
     """ Function Doc"""
-    with open(filename, 'aw', encoding="utf-8") as f:
+    with open(filename, 'a', encoding="utf-8") as f:
         sumChars = f.write(text)
         return sumChars
