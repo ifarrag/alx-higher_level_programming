b8a3bfe1718f99b95bd79780e12b574b2afe8622
ifarrag <ifarrrag@gmail.com>
2024-08-02 12:52:42 -0700

    json(fp=a.write())

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ def save_to_json_file(my_obj, filename):
     """ Function Doc"""
     import json
     with open(filename, 'w', encoding="utf-8") as f:
-        json.dump(my_obj, fp=f.write())
+        json.dump(my_obj, fp=f)
