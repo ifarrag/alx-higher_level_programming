272a682bb9be58a3b06ddb6c4fac12f8ffe1510b
ifarrag <ifarrrag@gmail.com>
2024-08-02 18:27:23 -0700

    json list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,19 +3,24 @@
 
 
 import sys
-save = __import__('5-save_to_json_file.py').save_to_json_file
-load = __import__('6-load_from_json_file.py').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
 
-f = open("add_item.json", 'a', encoding="utf-8")
+diss = 1
+f = open("add_item.json", 'a+', encoding="utf-8")
+if f.tell() == 0:
+    diss = 0
 f.close()
 
 listed = list()
-if sys.argv > 1:
+if len(sys.argv) > 1:
     for i in range(1, len(sys.argv)):
         listed.append(sys.argv[i])
 
-my_dir = load("add_item.json")
-
-my_dir += listed
+if diss == 1:
+    my_dir = load("add_item.json")
+    my_dir += listed
+else:
+    my_dir = listed
 
 save(my_dir, "add_item.json")
