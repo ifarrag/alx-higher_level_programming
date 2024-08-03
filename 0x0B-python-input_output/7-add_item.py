6e081e335b3532a645d138fc60b2b1074678d746
ifarrag <ifarrrag@gmail.com>
2024-08-02 17:41:27 -0700

    json list

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Module Doc"""
+
+
+import sys
+save = __import__("5-save_to_json_file.py").save_to_json_file
+load = __import__("6-load_from_json_file.py").load_from_json_file
+
+f = open("add_item.json", 'a', encoding="utf-8")
+f.close()
+
+if argv > 1:
+    listed = str(argv[1:])
+else:
+    listed = []
+
+my_dir = load("add_item.json")
+
+my_dir.append(listed)
+
+save(my_dir, "add_item.json")
