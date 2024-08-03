e70bc697679d72411079f103f2d795feb35603b5
ifarrag <ifarrrag@gmail.com>
2024-08-02 19:57:08 -0700

    dict

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module Doc"""
+
+
+class Student:
+    """ Class Doc"""
+
+    def __init__(self, first_name, last_name, age):
+        self.fname = first_name
+        self.lname = last_name
+        self.age = age
+
+    def to_json(self):
+        return {'first_name': self.fname, 'last_name': self.lname, 'age': self.age}
