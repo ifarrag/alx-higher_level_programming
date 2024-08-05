8be763b18e21351fe6dfe4bf9244ab373bd0967c
ifarrag <ifarrrag@gmail.com>
2024-08-05 15:51:01 -0700

    assertTrue

---
+++ 0x0C-python-almost_a_circle/tests/test_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module Doc"""
+
+
+import unittest
+class Test_all(unittest.TestCase):
+    """ Class Doc"""
+    def test_0(self):
+        """ Function Doc"""
+        self.assertTrue(True)
