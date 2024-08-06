74b1d9c68b4e883393941f5ebd4165665c59b0c4
ifarrag <ifarrrag@gmail.com>
2024-08-06 12:25:39 -0700

    jsonLoad

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,6 +17,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ Load string to json"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None:
             return json.dumps(list())
         return json.dumps(list_dictionaries)
