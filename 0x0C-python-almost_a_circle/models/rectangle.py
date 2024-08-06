42ec0d6de5c19714a2c0b8564f927d26ecab611e
ifarrag <ifarrrag@gmail.com>
2024-08-05 19:58:55 -0700

    __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,3 +85,8 @@ class Rectangle(Base):
                 for b in range(self.__width):
                     print("#", end="")
                 print()
+
+    def __str__(self):
+        return "[Rectangle]" + " " + "(" + str(self.id) + ")" + " " +
+                str(self.__x) + "/" + str(self.__y) + " " + "-" + " "
+                + str(self.__width) + "/" + str(self.__height)
