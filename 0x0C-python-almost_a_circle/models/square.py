af80e4c5d45e906dcbc6060fae815965f3d966f7
ifarrag <ifarrrag@gmail.com>
2024-08-06 11:15:46 -0700

    The square is special rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,13 +12,15 @@ class Square(Rectangle):
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        self.__size = value
+        self.width = value
+        self.height = value
 
     def __str__(self):
         return "[Square] (" + str(self.id) + ") "\
