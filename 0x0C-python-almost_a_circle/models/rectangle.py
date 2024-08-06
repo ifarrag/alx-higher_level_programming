a97ded23de765ec17d1dd6b59478cae6de0e060c
ifarrag <ifarrrag@gmail.com>
2024-08-05 20:08:18 -0700

    __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,5 +88,5 @@ class Rectangle(Base):
 
     def __str__(self):
         return "[Rectangle] (" + str(self.id) + ") "\
-                +str(self.__x) + "/" + str(self.__y) + " - "\
-                +str(self.__width) + "/" + str(self.__height)
+                + str(self.__x) + "/" + str(self.__y) + " - "\
+                + str(self.__width) + "/" + str(self.__height)
