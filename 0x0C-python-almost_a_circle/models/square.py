e82869e5a9326fdb3b3c9cd23b7fe07fdaf1a847
ifarrag <ifarrrag@gmail.com>
2024-08-06 10:42:01 -0700

    The square is special rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,7 +6,7 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """ Class Doc"""
     def __init__(self, size, x=0, y=0, id=None):
-        Rectangle().__init__(self, width=size, height=size, x, y, id)
+        Rectangle().__init__(self, size, size, x, y, id)
 
     def __str__(self):
         return "[Square] (" + str(self.id) + ") "\
