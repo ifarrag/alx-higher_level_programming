ff0b37179c8d6c3640e5938034a0641b8414daf1
ifarrag <ifarrrag@gmail.com>
2024-08-06 18:23:55 -0700

    file .json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,7 +24,7 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        fName = cls.__name__ + ".json"
-        wr_to = cls.to_json_string(list_objs)
-        with open(fName, 'w', encoding="utf-8") as f:
-            f.write(wr_to)
+        Name = cls.__name__ + ".json"
+        wrto = cls.to_json_string(list_objs)
+        with open(Name, 'w', encoding="utf-8") as f:
+            f.write(wrto)
